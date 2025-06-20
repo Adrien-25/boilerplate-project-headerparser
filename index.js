dd2743a09fbf825cbffe9f0f264380a51992b591
Adrien Schmidt <61613165+Adrien-25@users.noreply.github.com>
2025-06-20 22:09:21 +0000

    add api endpoint whoami

--- index.js
+++ index.js
@@ -2,29 +2,50 @@
 // where your node app starts
 
 // init project
-require('dotenv').config();
-var express = require('express');
+require("dotenv").config();
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require('cors');
+var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+// =========================================================
+// NEW: API endpoint for Request Header Parser Microservice
+// =========================================================
+app.get("/api/whoami", function (req, res) {
+  const ipaddress =
+    req.ip || req.connection.remoteAddress || req.headers["x-forwarded-for"];
+
+  // Get preferred language from the 'Accept-Language' header
+  const language = req.headers["accept-language"];
+
+  // Get software/operating system from the 'User-Agent' header
+  const software = req.headers["user-agent"];
+
+  // Send the information as a JSON object
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software,
+  });
 });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
